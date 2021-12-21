8d09538e1bb206c33d3fd206a9141e21662c5ef2
Ximing Chen <steven.xm824@gmail.com>
2021-12-21 10:41:59 -0800

    setup

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+
+
+
+const HomePage = () => {
+  return (
+  <div>
+    <div className='homeTitle'>
+      <h2>How dead You will be</h2>
+    </div>
+    <div className='naviBar'>
+      naviBar goes here
+    </div>
+  </div>
+  )
+}
+
+export default HomePage;
