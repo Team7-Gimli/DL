1c42a2c126f640fcae31f010ba8a2504b3c0beb6
Ximing Chen <steven.xm824@gmail.com>
2021-12-28 16:45:38 -0800

    able to upload

--- hooks/useStorage.js
+++ hooks/useStorage.js
@@ -1,30 +1,40 @@
-import {useState, useEffect} from 'react';
-import imageStorage from '../db/firebase/config.js';
+import { useState, useEffect } from 'react';
+import { imageStorage } from '../db/firebase/config.js';
+import { ref, uploadBytes } from 'firebase/storage';
 
 
 const useStorage = (file) => {
 
-  const [progress, setProgress] = useState (null);
-  const [err, setErr] = useState (null);
-  const [url, setUrl] = useState (null);
+  const [progress, setProgress] = useState(null);
+  const [err, setErr] = useState(null);
+  const [url, setUrl] = useState(null);
 
   const saveImg = () => {
     //references
-    const imgRef = imageStorage.ref(file.name);
-    imgRef.put(file).on('state_changed', (snap) => {
-      let precentage = (snap.bytesTransferred / snap.totalBytes) * 100;
-      setProgress(precentage);
-    }, (error) => {
-      setErr(error);
-    }, async () => {
-      const url = await imgRef.getDownloadURL();
-      setUrl(url);
-    } )
+    const imgRef = ref(imageStorage, file.name);
+
+    uploadBytes(imgRef, file)
+      .then( async snap => {
+        console.log('upload bolb');
+        let precentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+        setProgress(precentage);
+      })
+      .catch(error => setErr(error))
+
+    // imgRef.put(file).on('state_changed', (snap) => {
+    //   let precentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+    //   setProgress(precentage);
+    // }, (error) => {
+    //   setErr(error);
+    // }, async () => {
+    //   const url = await imgRef.getDownloadURL();
+    //   setUrl(url);
+    // })
   }
 
-  useEffect( saveImg , [file]);
+  useEffect(saveImg, [file]);
 
-  return {progress, url, err};
+  return { progress, url, err };
 }
 
 export default useStorage;
