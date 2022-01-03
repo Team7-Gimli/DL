83e909847d1c8f4deee95d320a10f3ca11bb7447
Ximing Chen <steven.xm824@gmail.com>
2022-01-03 10:32:25 -0800

    complete img click

--- components/imageCard.js
+++ components/imageCard.js
@@ -2,9 +2,13 @@ import { Card, Image } from 'react-bootstrap';
 
 const ImageCard = ({ img }) => {
 
+  const imgClick = () => {
+    window.open (img);
+  }
+
   return (
     <Card style={{ width: '18rem' }}>
-      <Image src={img} />
+      <Image onClick={imgClick} src={img} />
     </Card>
   )
 }
